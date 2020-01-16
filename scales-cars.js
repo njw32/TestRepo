f507cbf59c4aafce9c637fd146159792d46d62ed
unknown <njw32@students.calvin.edu>
2020-01-16 18:49:34 -0500

    new axis

--- scales-cars.js
+++ scales-cars.js
@@ -1,42 +1,84 @@
-console.log(Math.min(...cars.map(d => d.mpg)));
-console.log(d3.min(cars.map(d => d.mpg)));
-console.log(d3.max(cars.map(d => d.mpg)));
-console.log(d3.extent(cars.map(d => d.mpg)));
+let outerWidth = 600
+let outerHeight = 400
+let margins = { top: 30, bottom: 50, left: 50, right: 30 }
+let innerWidth = outerWidth - margins.left - margins.right
+let innerHeight = outerHeight - margins.top - margins.bottom
 
-//d3.csv("cars-data.csv").then(draw);
-d3.select("svg#cars-scatter")
-  .style("width", 600)
-  .style("height", 400);
+let scatterOuter = d3
+  .select('svg#cars-scatter')
+  .attr('width', outerWidth)
+  .attr('height', outerHeight)
 
-let dispScale = d3
-  .scaleLinear()
-  .domain(d3.extent(cars.map(d => d.disp)))
-  .range([50, 350]);
+let scatterInner = scatterOuter
+  .append('g')
+  .attr('width', innerWidth)
+  .attr('height', innerHeight)
+  .attr('transform', 'translate(' + margins.left + ',' + margins.right + ')')
 
-let mpgScale = d3
+scatterOuter
+  .append('rect')
+  .attr('width', outerWidth)
+  .attr('height', outerHeight)
+  .attr('fill', 'transparent')
+  .attr('stroke', 'black')
+
+scatterInner.style('fill', 'skyblue').style('opacity', 0.8)
+
+scatterInner
+  .append('rect')
+  .attr('width', innerWidth)
+  .attr('height', innerHeight)
+  .attr('fill', 'skyblue')
+  .attr('fill-opacity', 0.2)
+
+let xScale = d3
   .scaleLinear()
   .domain(d3.extent(cars.map(d => d.mpg)))
-  .range([300, 50]);
+  .range([20, innerWidth - 20])
+let xAxis = d3.axisBottom(xScale)
 
-let dotScale = d3
-  .scaleSqrt()
+let yScale = d3
+  .scaleLinear()
   .domain(d3.extent(cars.map(d => d.wt)))
-  .range([0, d3.max(cars.map(d => d.wt)) + 20]);
+  .range([20, innerHeight - 20].reverse())
+let yAxis = d3.axisLeft(yScale).tickSize(-innerWidth)
+
+let sizeScale = d3
+  .scaleSqrt()
+  .domain([0, Math.max(...cars.map(d => d.disp))])
+  .range([5, 15])
 
 let colorScale = d3
-  .scaleSequential()
-  .domain(d3.extent(cars.map(d => d.cyl)))
-  .interpolator(d3.interpolateCool);
+  .scaleOrdinal()
+  .domain([4, 6, 8])
+  .range(['red', 'green', 'blue'])
 
-d3.select("svg#cars-scatter")
-  .selectAll("circle")
+scatterInner
+  .append('g')
+  .attr('transform', 'translate(' + 0 + ', ' + innerHeight + ')')
+  .attr('class', 'x-axis')
+  .call(xAxis)
+
+scatterInner
+  .append('rect')
+  .attr('width', innerWidth)
+  .attr('height', innerHeight)
+  .attr('fill', 'transparent')
+  .attr('stroke', 'black')
+
+scatterInner
+  .append('g')
+  .attr('class', 'y-axis')
+  .call(yAxis)
+
+scatterInner
+  .selectAll('circle')
   .data(cars)
   .enter()
-  .append("circle")
-  .attr("cx", d => dispScale(d.disp))
-  .attr("cy", d => mpgScale(d.mpg))
-  .attr("r", d => dotScale(d.wt))
-  .style("fill", d => colorScale(d.cyl))
-  .style("stroke", "red");
-
-console.log(cars.length);
+  .append('circle')
+  .attr('cx', d => xScale(d.mpg))
+  .attr('cy', d => yScale(d.wt))
+  .style('fill', 'transparent')
+  .style('stroke-width', 3)
+  .attr('r', d => sizeScale(d.disp))
+  .style('stroke', d => colorScale(d.cyl))
